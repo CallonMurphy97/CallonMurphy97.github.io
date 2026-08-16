f0112070ba5dbd69d7c050499e01c544aeb4a5d4
CallonMurphy97 <46601015+CallonMurphy97@users.noreply.github.com>
2026-08-15 22:43:43 -0400

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-var mainViews = ['about','filmography','otherProj'];
+var mainViews = ['about','filmography','performances'];
 
 class Film {
   constructor(name, date, runtime, format, description, screenings, video, laurels, images)
@@ -15,16 +15,95 @@ class Film {
   }
 }
 
+class Performance {
+  constructor(name, date, desc, video)
+  {
+    this.name = name;
+    this.date = date;
+    this.desc = desc;
+    this.video = video;
+  }
+}
+
 function showView(viewID)
 {
    document.getElementById(viewID).style.display = 'inline';
+   document.getElementById(viewID + "A").classList.add("active");
    for( let i = 0; i < mainViews.length; i++ )
    {
      if( mainViews[i] != viewID )
      {
+       document.getElementById(mainViews[i] + "A").classList.remove("active");
        document.getElementById(mainViews[i]).style.display = 'none';
      }
    }
+}
+
+function myFunction() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
+
+function openNav() {
+  document.getElementById("mySideNav").style.width = "250px";
+  document.getElementById("filmMain").style.marginLeft = "260px";
+}
+
+function closeNav() {
+  document.getElementById("mySideNav").style.width = "0px";
+  document.getElementById("filmMain").style.marginLeft = "10px";
+}
+
+function showFilmography(title)
+{
+  showFilm(title);
+  showView("filmography");
+}
+
+function createFilmCard(title)
+{
+  film = filmMap.get(title);
+  let filmCardDef = document.createElement("div");
+  filmCardDef.style = "display: flex;justify-content: center;";
+  //filmCardDef.onClick
+  filmCardDef.innerHTML = filmCardDef.innerHTML + `
+  <div class="card">
+    <a href="#" onclick="showFilmography('` + title + `')">
+      <img class="cardImg" src=` + film.images[0] + `>
+      <div class="cardMiddle"> 
+        <div class="cardText">` + title + `</div>
+      </div>
+    </a>
+  </div>`;
+  return filmCardDef;
+}
+
+function buildFilmCards()
+{
+    let filmCards = document.createElement("div");
+    filmCards.style = "display: flex;justify-content: center;flex-wrap: wrap;";
+    filmCards.innerHTML = "";
+
+    let maxFilms = 6;
+    let filmCount = 0;
+    for (let [key, value] of filmMap.entries())
+    {
+      if( filmCount < maxFilms)
+      {
+        let filmCard = createFilmCard(key);
+        filmCards.appendChild(filmCard);
+      }
+      filmCount++;
+    }
+    
+    let cardDiv = document.getElementById("filmCards");
+    cardDiv.innerHTML = '';
+    cardDiv.appendChild(filmCards);
+    cardDiv.innerHTML = cardDiv.innerHTML + `<br></div>`;
 }
 
   function showFilm(title)
@@ -32,8 +111,9 @@ function showView(viewID)
     film = filmMap.get(title);
     let screenWidth = document.documentElement.clientWidth;
     let filmContainerDef = document.createElement("div");
+    filmContainerDef.style = "min-width: 25vw;";
     let titleContainer = document.createElement("h4");
-    titleContainer.innerHTML = `<b>` + film.name + `</b>`;
+    titleContainer.innerHTML = `<b><i>` + film.name + `</i></b>`;
     filmContainerDef.appendChild(titleContainer);
 
     if( film.video != null && film.video.length == 3 )
@@ -47,21 +127,25 @@ function showView(viewID)
         divStyle = "padding: 75% 0 0 0;position:relative;";
         vidPos = "position:absolute;";
       }
+      else if( film.video[1]  > 400)
+      {
+        let descWidth = film.video[1];
+        filmContainerDef.style = "min-width: " + descWidth + "px;";
+      }
         let videoContainer = document.createElement("div");
         videoContainer.innerHTML = videoContainer.innerHTML + `<div style="`+ divStyle +`"><iframe src="` + film.video[0] + `" frameborder="0" allow="autoplay; fullscreen; picture-in-picture; clipboard-write; encrypted-media; web-share" referrerpolicy="strict-origin-when-cross-origin" style="`+ vidPos + `top:0;left:0;` + vidDim +  `" title="Twin City Twist (2023)"></iframe></div><br>`;
         filmContainerDef.appendChild(videoContainer);
     }  
 
     filmContainerDef.innerHTML = filmContainerDef.innerHTML + `
-      <div class="film">
-        <p style="font-size:90%;max-width: 1000px;'">` + film.description + `</p>
+      <div class="film" style="font-size: 90%;max-width: 1000px;">
+        <p>` + film.description + `</p>
         <br>
-        <h5><i>` + film.date + ` - ` + film.runtime + ` mins - ` + film.format + `</i></h5><p></div>`;
+        <h5><i>` + film.date + ` - ` + film.runtime + ` mins - ` + film.format + `</i></h5></div>`;
 
     let filmContainerVidAndStills = document.createElement("div");                                                                                                     
-
     let imgScale = "width:25%;height:25%";
-    if( screenWidth < 700 )
+    if( screenWidth < 700 || film.images.length <= 4)
     {
       imgScale = "width:50%;height:50%;"
     }
@@ -83,10 +167,12 @@ function showView(viewID)
     {
       filmContainerVidAndStills.innerHTML =  filmContainerVidAndStills.innerHTML + `<br>`;
     }
+    filmContainerVidAndStills.innerHTML = filmContainerVidAndStills.innerHTML + `</div>`;
 
+    let filmScreenings = document.createElement("div");
     if( film.screenings != null )
     {
-      filmContainerVidAndStills.innerHTML =  filmContainerVidAndStills.innerHTML + `<br><h5><b>Screenings:</b></h5>`;
+      filmScreenings.innerHTML = filmScreenings.innerHTML +  `<br><h5><b>Screenings:</b></h5>`;
 
       var screeningTable = document.createElement("TABLE");
       for( let i = 0; i < film.screenings.length; i++ )
@@ -99,25 +185,60 @@ function showView(viewID)
           row.cells[j].style.textAlign = 'right';
         }
       }
+      filmScreenings.appendChild(screeningTable);
     }
-    filmContainerVidAndStills.innerHTML = filmContainerVidAndStills.innerHTML + `</div>`;
+    filmScreenings.innerHTML = filmScreenings.innerHTML +  `<br></div>`;
+    filmContainerDef.appendChild(filmScreenings);
 
     // Find the ID 'user-list' and append the userContainer to it.
     // This will cause it to display on the page.
     let filmMain = document.getElementById("filmMain");
-    filmMain.innerHTML = '';
-    filmMain.appendChild(filmContainerDef);
-    filmMain.appendChild(filmContainerVidAndStills);
-    if( screeningTable != null )
+
+    let filmInfoAndStills = document.createElement("div");
+    if( screenWidth > 700 )
     {
-      filmMain.appendChild(screeningTable);
+      filmInfoAndStills.style = "display: flex;justify-content: center;align-items: start;";
+      filmContainerVidAndStills.style = "margin-left: 20px;margin-top: 50px;";
     }
+    filmInfoAndStills.innerHTML = '';
+    filmInfoAndStills.appendChild(filmContainerDef);
+    filmInfoAndStills.appendChild(filmContainerVidAndStills);
+
+    filmMain.innerHTML = '';
+    filmMain.appendChild(filmInfoAndStills);
+
+    closeNav();
   }
 
-  
+  function showPerformances()
+  {
+    showView("performances");
+
+      let performancesDef = document.getElementById("performances");
+      performancesDef.style = "text-align: center;align-content: center;width: 100%;margin: 0 auto; max-width: 800px;";
+      performancesDef.innerHTML = "";
+      for( let [key, value] of perfMap.entries() )
+      {
+        perf = value;
+        let perfDef = document.createElement("div");
+        perfDef.innerHTML = "";
+        perfDef.innerHTML = perfDef.innerHTML + `<h2>` + perf.name + `</h2>` 
+                            + `<h4>` + perf.date + `</h4><p>` + perf.desc + `</p>`
+                            + perf.video + `<br></div>`;
+        performancesDef.appendChild(perfDef);
+      }
+  }
+
+
 const filmMap = new Map();
 for( let i = 0; i < FILMS.length; i++ )
 {
   filmMap.set(FILMS[i][0], new Film(FILMS[i][0], FILMS[i][1], FILMS[i][2], FILMS[i][3], FILMS[i][4], FILMS[i][5], FILMS[i][6], FILMS[i][7], FILMS[i][8]) );
 }
+const perfMap = new Map();
+for( let i = 0; i < PERFORMANCES.length; i++ )
+{
+  perfMap.set(PERFORMANCES[i][0], new Performance(PERFORMANCES[i][0], PERFORMANCES[i][1], PERFORMANCES[i][2], PERFORMANCES[i][3]) );
+}
 window.onload = showFilm("Tactile Light Diary");
+window.onload = buildFilmCards();
